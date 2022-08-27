a466afb07b7cb8f1d6ee74fd1c171bf174338a35
M. Bagher Abiat <zorofight94@gmail.com>
2022-08-27 10:22:42 +0430

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import Bench from './bench';
-import Task from './task';
+export type {} from "../@types/index";
+import Bench from "./bench";
+import Task from "./task";
 
-export { now } from './utils';
+export { now } from "./utils";
 
 export { Bench, Task };
 export default Bench;

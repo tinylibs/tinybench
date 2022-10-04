ab64db6e3005bdd1c28208d2971d96f2f7be95c8
Mohammad Bagher Abiat <zorofight94@gmail.com>
2022-10-04 17:41:31 +0330

    Revert "init"
    This reverts commit a8dcffc071d38425885530c2037533736b775894.

--- test/sequential.test.ts
+++ test/sequential.test.ts
@@ -3,7 +3,28 @@ import { test, expect } from 'vitest';
 import { Bench } from '../src';
 
 test('sequential', async () => {
+  const bench = new Bench({
+    time: 0,
+    iterations: 100,
+  });
+
   let isFirstTaskDefined = true;
+  bench
+    .add('sample 1', async () => {
+      await setTimeout(0);
+      for (let i = 0; i < 1e7; i++);
+    })
+    .add('sample 2', async () => {
+      if (typeof bench.tasks[0]?.result === 'undefined') isFirstTaskDefined = false;
+
+      await setTimeout(0);
+      for (let i = 0; i < 1e7; i++);
+    });
+
+  await bench.warmup();
+  await bench.run();
+  // first task should be undefined sometimes because of concurrency
+  expect(isFirstTaskDefined).toBe(false);
 
   const sequentialBench = new Bench({
     time: 0,
@@ -18,8 +39,7 @@ test('sequential', async () => {
     })
     .add('sample 2', async () => {
       // sample 1 should be defined always
-      if (typeof sequentialBench.tasks[0]?.result === 'undefined')
-        isFirstTaskDefined = false;
+      if (typeof sequentialBench.tasks[0]?.result === 'undefined') isFirstTaskDefined = false;
       else isFirstTaskDefined = true;
 
       await setTimeout(0);
@@ -27,6 +47,13 @@ test('sequential', async () => {
     });
 
   await sequentialBench.warmup();
-  await sequentialBench.run();
+  await sequentialBench.runSequentially();
   expect(isFirstTaskDefined).toBe(true);
+
+  expect(sequentialBench.tasks[0]!.result!.mean).toBeLessThan(
+    bench.tasks[0]!.result!.mean,
+  );
+  expect(sequentialBench.tasks[1]!.result!.mean).toBeLessThan(
+    bench.tasks[1]!.result!.mean,
+  );
 });

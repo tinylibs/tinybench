70019a2fcb5fd4158cb510efc04b6ffb19b94a48
Morteza-Mazrae <mor.mazrae@gmail.com>
2022-10-17 23:52:14 +0330

    rewrite sort function

--- src/task.ts
+++ src/task.ts
@@ -73,7 +73,7 @@ export default class Task extends EventTarget {
     }
     await this.bench.teardown(this, 'run');
 
-    samples.sort();
+    samples.sort((a,b)=> a - b);
 
     {
       const min = samples[0]!;

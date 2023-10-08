da318179a9beba83439e8a3f2bd7b3f2d674fc21
Dunqing <dengqing0821@gmail.com>
2023-10-08 11:16:03 +0800

    perf: avoid calling `sort()` and multiple use `samples.length`

--- src/task.ts
+++ src/task.ts
@@ -57,6 +57,8 @@ export default class Task extends EventTarget {
   async run() {
     this.dispatchEvent(createBenchEvent('start', this));
     let totalTime = 0; // ms
+    let min = Infinity;
+    let max = -Infinity;
     const samples: number[] = [];
 
     await this.bench.setup(this, 'run');
@@ -90,9 +92,11 @@ export default class Task extends EventTarget {
           taskTime = this.bench.now() - taskStart;
         }
 
-        samples.push(taskTime);
         this.runs += 1;
         totalTime += taskTime;
+        samples.push(taskTime);
+        if (taskTime > max) max = taskTime;
+        if (taskTime < min) min = taskTime;
 
         if (this.opts.afterEach != null) {
           await this.opts.afterEach.call(this);
@@ -112,29 +116,26 @@ export default class Task extends EventTarget {
 
     await this.bench.teardown(this, 'run');
 
-    samples.sort((a, b) => a - b);
-
     if (!this.result?.error) {
-      const min = samples[0]!;
-      const max = samples[samples.length - 1]!;
       const period = totalTime / this.runs;
       const hz = 1000 / period;
 
       // benchmark.js: https://github.com/bestiejs/benchmark.js/blob/42f3b732bac3640eddb3ae5f50e445f3141016fd/benchmark.js#L1912-L1927
+      const simplesLength = samples.length;
       const mean = getMean(samples);
       const variance = getVariance(samples, mean);
       const sd = Math.sqrt(variance);
-      const sem = sd / Math.sqrt(samples.length);
-      const df = samples.length - 1;
+      const sem = sd / Math.sqrt(simplesLength);
+      const df = simplesLength - 1;
       const critical = tTable[String(Math.round(df) || 1)] || tTable.infinity!;
       const moe = sem * critical;
       const rme = (moe / mean) * 100 || 0;
 
       // mitata: https://github.com/evanwashere/mitata/blob/3730a784c9d83289b5627ddd961e3248088612aa/src/lib.mjs#L12
-      const p75 = samples[Math.ceil(samples.length * (75 / 100)) - 1]!;
-      const p99 = samples[Math.ceil(samples.length * (99 / 100)) - 1]!;
-      const p995 = samples[Math.ceil(samples.length * (99.5 / 100)) - 1]!;
-      const p999 = samples[Math.ceil(samples.length * (99.9 / 100)) - 1]!;
+      const p75 = samples[Math.ceil(simplesLength * (75 / 100)) - 1]!;
+      const p99 = samples[Math.ceil(simplesLength * (99 / 100)) - 1]!;
+      const p995 = samples[Math.ceil(simplesLength * (99.5 / 100)) - 1]!;
+      const p999 = samples[Math.ceil(simplesLength * (99.9 / 100)) - 1]!;
 
       if (this.bench.signal?.aborted) {
         return this;

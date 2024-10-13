96f861fd54f53e10d9946088ea0568b83703677a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-10-13 15:17:52 +0200

    refactor!: remove non working benchmark todos
    closes #62
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- examples/src/simple.ts
+++ examples/src/simple.ts
@@ -10,8 +10,7 @@ bench
   .add('slower task', async () => {
     await new Promise((r) => setTimeout(r, 1)); // we wait 1ms :)
     console.log('I am slower');
-  })
-  .todo('unimplemented bench');
+  });
 
 await bench.run();
 
@@ -24,16 +23,3 @@ console.table(bench.table());
 // │ 0       │ 'faster task' │ '38,832' │ 25751.297631307978   │ '±3.48%' │ '22016.49999997812±5.5000000145' │ 3884    │
 // │ 1       │ 'slower task' │ '669'    │ 1493338.567164177    │ '±5.98%' │ '1445076.0000000286'             │ 67      │
 // └─────────┴───────────────┴──────────┴──────────────────────┴──────────┴──────────────────────────────────┴─────────┘
-
-console.table(
-  bench.todos.map(({ name }) => ({
-    'Task name': name,
-  })),
-);
-
-// Output:
-// ┌─────────┬───────────────────────┐
-// │ (index) │       Task name       │
-// ├─────────┼───────────────────────┤
-// │    0    │ 'unimplemented bench' │
-// └─────────┴───────────────────────┘

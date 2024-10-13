b1cbf0433b24f465c0171f8241e06686ba9cadb9
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-10-13 11:59:15 +0200

    fix: silence tsup exports warning (#114)
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  outDir: "dist",
-  format: ["esm", "cjs"],
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
   minify: false,
   minifySyntax: true,
   minifyWhitespace: false,

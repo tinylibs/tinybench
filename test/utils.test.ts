90a8af71779d7609a1a011e91930de4f4aca5b20
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-01-29 13:00:24 +0100

    docs(test): add link to median absolute deviation computation
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -3,6 +3,7 @@ import { expect, test } from 'vitest'
 import { getStatisticsSorted } from '../src/utils'
 
 test('median absolute deviation', () => {
+  // https://www.wolframalpha.com/input?i=MedianDeviation[1,2,3]
   const stats = getStatisticsSorted([1, 2, 3])
   expect(stats.mad).toBe(1)
 })

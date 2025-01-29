102bb80d29ed8eac03eb2b41305f7194a840e78e
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-01-29 19:06:08 +0100

    Update test/index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -164,7 +164,7 @@ test('bench table (sync)', () => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       'Latency average (ns)': expect.any(String),
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      'Latency median (ns)': expect.any(Number),
+      'Latency median (ns)': expect.any(String),
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       'Throughput average (ops/s)': expect.any(String),
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment

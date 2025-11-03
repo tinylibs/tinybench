966b9f97021d917101c092484c0289670600d17b
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-03 20:28:22 +0100

    fix error tests for bun

--- test/utils-to-error.test.ts
+++ test/utils-to-error.test.ts
@@ -5,10 +5,12 @@ import {
 } from '../src/utils'
 
 test('toError - null', () => {
-  expect(toError(null)).toStrictEqual(new Error())
+  expect(toError(null)).toBeInstanceOf(Error)
+  expect(toError(null).message).toBe('')
 })
 test('toError - undefined', () => {
-  expect(toError(undefined)).toStrictEqual(new Error())
+  expect(toError(undefined)).toBeInstanceOf(Error)
+  expect(toError(undefined).message).toBe('')
 })
 
 test('toError - Error', () => {
@@ -17,34 +19,42 @@ test('toError - Error', () => {
 })
 
 test('toError - string', () => {
-  expect(toError('test error')).toStrictEqual(new Error('test error'))
+  expect(toError('test error')).toBeInstanceOf(Error)
+  expect(toError('test error').message).toBe('test error')
 })
 
 test('toError - object with message', () => {
-  expect(toError({ message: 'test error' })).toStrictEqual(new Error('test error'))
+  expect(toError({ message: 'test error' })).toBeInstanceOf(Error)
+  expect(toError({ message: 'test error' }).message).toBe('test error')
 })
 
 test('toError - object without message', () => {
-  expect(toError({ foo: 'bar' })).toStrictEqual(new Error())
+  expect(toError({ foo: 'bar' })).toBeInstanceOf(Error)
+  expect(toError({ foo: 'bar' }).message).toBe('')
 })
 
 test('toError - number', () => {
-  expect(toError(42)).toStrictEqual(new Error('42'))
+  expect(toError(42)).toBeInstanceOf(Error)
+  expect(toError(42).message).toBe('42')
 })
 
 test('toError - boolean', () => {
-  expect(toError(true)).toStrictEqual(new Error('true'))
+  expect(toError(true)).toBeInstanceOf(Error)
+  expect(toError(true).message).toBe('true')
 })
 
 test('toError - symbol', () => {
-  expect(toError(Symbol('test'))).toStrictEqual(new Error('Symbol(test)'))
+  expect(toError(Symbol('test'))).toBeInstanceOf(Error)
+  expect(toError(Symbol('test')).message).toBe('Symbol(test)')
 })
 
 test('toError - function', () => {
   const errorFunction = () => { throw new Error('errorFunction') }
-  expect(toError(errorFunction)).toStrictEqual(new Error('errorFunction'))
+  expect(toError(errorFunction)).toBeInstanceOf(Error)
+  expect(toError(errorFunction).message).toBe('errorFunction')
 })
 
 test('toError - array', () => {
-  expect(toError(['error message'])).toStrictEqual(new Error())
+  expect(toError(['error message'])).toBeInstanceOf(Error)
+  expect(toError(['error message']).message).toBe('')
 })

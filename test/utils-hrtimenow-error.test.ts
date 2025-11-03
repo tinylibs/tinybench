f4176f72aa4a934b30e1d28c38af08814bbd3c8d
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-03 20:58:42 +0100

    fix hrtimeNow coverage

--- test/utils-hrtimenow-error.test.ts
+++ test/utils-hrtimenow-error.test.ts
@@ -3,8 +3,8 @@ import { expect, test } from 'vitest'
 test('hrtimeNow - process.hrtime undefined', async () => {
   // @ts-expect-error set to undefined to simulate unsupported environment
   process.hrtime = undefined
-  // @ts-expect-error we use a query param to avoid loading cached module
-  const hrtimeNow = (await import('../src/utils?in_test=1') as { hrtimeNow: () => number }).hrtimeNow
+
+  const hrtimeNow = (await import('Bun' in globalThis ? '../src/utils?in_test=1' : '../src/utils') as { hrtimeNow: () => number }).hrtimeNow
 
   expect(typeof hrtimeNow).toBe('function')
   expect(() => hrtimeNow()).toThrowError(new Error('hrtime.bigint() is not supported in this JS environment'))

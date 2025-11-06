5f9f38e9acb2dd1042af295fbd281ec375b5674d
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-06 14:30:45 +0100

    fix: size-limit

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'tsdown/config'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  outExtensions: () =>{
-    return {
-      js: '.js',
-      dts: '.d.ts',
-    }
-  },
   minify: {
     compress: true,
     mangle: true,
+  },
+  outExtensions: () => {
+    return {
+      dts: '.d.ts',
+      js: '.js',
+    }
   },
 })

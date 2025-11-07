586b24e49f921b11b9b6450d22b31263681d2aed
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-07 12:09:28 +0100

    check if not timing out

--- test/utils.ts
+++ test/utils.ts
@@ -1,4 +1,4 @@
-const nil = typeof SharedArrayBuffer !== 'undefined' && typeof Atomics !== 'undefined' && new Int32Array(new SharedArrayBuffer(4))
+const nil32 = typeof SharedArrayBuffer !== 'undefined' && typeof Atomics !== 'undefined' && new Int32Array(new SharedArrayBuffer(4))
 
 /**
  * Synchronously blocks the event loop for the specified number of milliseconds.
@@ -6,9 +6,11 @@ const nil = typeof SharedArrayBuffer !== 'undefined' && typeof Atomics !== 'unde
  * Use only in test or non-production code.
  * @param ms amount of time to sleep in milliseconds
  */
-export const sleep = nil !== false
+export const sleep = nil32 !== false
   ? (ms: number): void => {
-      Atomics.wait(nil, 0, 0, ms)
+      if (Atomics.wait(nil32, 0, 0, ms) !== 'timed-out') {
+        throw new Error('Atomics.wait returned unexpected value')
+      }
     }
   : (ms: number): void => {
       const target = performance.now() + ms

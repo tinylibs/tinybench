49e552babd0bdf123964c9c675684daed8ea30d5
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-07 11:46:33 +0100

    test: use atomic sleep

--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,18 @@
+const nil = typeof SharedArrayBuffer !== 'undefined' && typeof Atomics !== 'undefined' && new Int32Array(new SharedArrayBuffer(4))
+
 /**
  * Synchronously blocks the event loop for the specified number of milliseconds.
  * This is a busy-wait sleep function (not async, not Promise-based).
  * Use only in test or non-production code.
  * @param ms amount of time to sleep in milliseconds
  */
-export const sleep = (ms: number): void => {
-  const start = performance.now()
-  while (performance.now() - start < ms) {
-    // noop
-  }
-}
+export const sleep = nil !== false
+  ? (ms: number): void => {
+      Atomics.wait(nil, 0, 0, ms)
+    }
+  : (ms: number): void => {
+      const start = performance.now()
+      while (performance.now() - start < ms) {
+      // noop
+      }
+    }

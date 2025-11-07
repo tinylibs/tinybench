184528b6eee362b3eb64b984660f54e098084d22
Aras Abbasi <aras.abbasi@googlemail.com>
2025-11-07 12:55:16 +0100

    only use atomics on linux

--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,20 @@
 const nil32 = typeof SharedArrayBuffer !== 'undefined' && typeof Atomics !== 'undefined' && new Int32Array(new SharedArrayBuffer(4))
 
+const platform = await (async () => {
+  try {
+    return await import('node:os').then(mod => mod.platform())
+  } catch {
+    return 'unknown'
+  }
+})()
+
 /**
  * Synchronously blocks the event loop for the specified number of milliseconds.
  * This is a busy-wait sleep function (not async, not Promise-based).
  * Use only in test or non-production code.
  * @param ms amount of time to sleep in milliseconds
  */
-export const sleep = nil32 !== false
+export const sleep = nil32 !== false && platform === 'linux'
   ? (ms: number): void => {
       if (Atomics.wait(nil32, 0, 0, ms) !== 'timed-out') {
         throw new Error('Atomics.wait returned unexpected value')

b3faefc566ae90cfddcbe317f79b8f6421afcefc
Ari Perkkiö <ari.perkkio@gmail.com>
2026-05-07 11:50:08 +0300

    build: do not minify published code

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'tsdown/config'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  minify: {
-    compress: true,
-    mangle: true,
-  },
   outExtensions: () => {
     return {
       dts: '.d.ts',

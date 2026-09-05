a1ea62224986778ecb45d9cfd3eb1959b3ebd60f
michalius <48828911+michalius@users.noreply.github.com>
2026-09-05 13:09:42 +0200

    fix: avoid globalThis.Event type leak

--- src/event.ts
+++ src/event.ts
@@ -14,7 +14,7 @@ import type {
 class BenchEvent<
   K extends BenchEvents = BenchEvents,
   M extends 'bench' | 'task' = 'bench'
-> extends globalThis.Event {
+> extends Event {
   declare type: K
 
   /**
